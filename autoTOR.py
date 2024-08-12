3d11347f88265e7754a5051e5f7fc126b74231d5
Glaucio Fonseca <163554650+glauciofonsecaa@users.noreply.github.com>
2024-08-12 15:04:13 -0300

    Update autoTOR.py

--- autoTOR.py
+++ autoTOR.py
@@ -13,7 +13,7 @@ try:
         pass
 except subprocess.CalledProcessError:
     print('[+] pip3 not installed')
-    subprocess.check_output('sudo apt update',shell=True)
+    subprocess.check_output('sudo apt update', shell=True)
     subprocess.check_output('sudo apt install python3-pip -y', shell=True)
     print('[!] pip3 installed succesfully')
 
@@ -26,16 +26,16 @@ except Exception:
     print('[+] python3 requests is not installed')
     os.system('pip3 install requests')
     os.system('pip3 install requests[socks]')
-    print('[!] python3 requests is installed ')
+    print('[!] python3 requests is installed')
 try:
 
     check_tor = subprocess.check_output('which tor', shell=True)
 except subprocess.CalledProcessError:
 
     print('[+] tor is not installed !')
-    subprocess.check_output('sudo apt update',shell=True)
-    subprocess.check_output('sudo apt install tor -y',shell=True)
-    print('[!] tor is installed succesfully ')
+    subprocess.check_output('sudo apt update', shell=True)
+    subprocess.check_output('sudo apt install tor -y', shell=True)
+    print('[!] tor is installed succesfully')
 
 os.system("clear")
 def ma_ip():
@@ -67,9 +67,9 @@ os.system("service tor start")
 time.sleep(3)
 print("\033[1;32;40m change your  SOCKES to 127.0.0.1:9050 \n")
 os.system("service tor start")
-x = input("[+] time to change Ip in Sec [type=60] >> ")
+x = input("[+] time to change Ip in Sec [type=60] >>")
 lin = input("[+] how many time do you want to change your ip [type=1000]for infinte ip change type [0] >>")
-if int(lin) ==int(0):
+if int(lin) == int(0):
 
 	while True:
 		try:
